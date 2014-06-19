5ba5392bca9082c3362cdefa4e0858ef970b57c9
Jonathan J Hunt <j@me.net.nz>
2014-06-19 11:13:54 -0700

    Fix mix.

--- mix.exs
+++ mix.exs
@@ -6,16 +6,25 @@ defmodule Mock.Mixfile do
       name: "Mock",
       source_url: "https://github.com/jjh42/mock",
       homepage_url: "http://jjh42.github.io/mock",
-      version: "0.0.3",
-      env: [
-          dev: [deps: deps ++ dev_deps],
-          test: [deps: deps] ,
-          prod: [deps: deps]]]
+      version: "0.0.4",
+      deps: deps ]
+  end
+
+  defp deps do
+    deps(Mix.env)
+  end
+
+  defp deps(:dev) do
+    prod_deps ++ dev_deps
+  end
+
+  defp deps(_) do
+    prod_deps
   end
 
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  defp deps do
+  defp prod_deps do
     [ {:meck,"0.8.2", [github: "eproxus/meck", tag: "0.8.2"]}]
   end
 
