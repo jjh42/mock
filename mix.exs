be50ad5c72a604ef05906566fde2a20bbb7f37e7
Jonathan J Hunt <j@me.net.nz>
2014-06-19 00:24:19 -0700

    Updated dependencies.

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Mock.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ {:meck,"0.7.2", [github: "eproxus/meck", tag: "0.7.2"]}]
+    [ {:meck,"0.8.2", [github: "eproxus/meck", tag: "0.8.2"]}]
   end
 
   # Dependencies only needed during development.
