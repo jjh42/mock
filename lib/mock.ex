34c5f22c3370c26ad8250fee81c90dcad8e354d5
Jonathan J Hunt <j@me.net.nz>
2014-06-19 00:04:30 -0700

    Switch to new 0.14 style default arguments.

--- lib/mock.ex
+++ lib/mock.ex
@@ -37,7 +37,7 @@ defmodule Mock do
          assert called HTTPotion.get("http://example.com")
       end
   """
-  defmacro with_mock(mock_module, opts // [], mocks, test) do
+  defmacro with_mock(mock_module, opts \\ [], mocks, test) do
     quote do
       :meck.new(unquote(mock_module), unquote(opts))
       unquote(__MODULE__)._install_mock(unquote(mock_module), unquote(mocks))
