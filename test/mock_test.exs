02d9b5e6aba474bcb5b196cb4671ee7dc9b99764
Jonathan J Hunt <j@me.net.nz>
2014-06-19 12:17:52 -0700

    Fixed tests to not use a non-existant module.

--- test/mock_test.exs
+++ test/mock_test.exs
@@ -1,41 +1,42 @@
 Code.require_file "test_helper.exs", __DIR__
 
+
 defmodule MockTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   import Mock
 
   test "simple mock" do
-    with_mock Dummy,
-        [foo: fn(x) -> 2*x end] do
-      assert Dummy.foo(3) == 6
+    with_mock String,
+        [reverse: fn(x) -> 2*x end] do
+      assert String.reverse(3) == 6
     end
   end
 
   test "called" do
-    with_mock Dummy,
-       [foo: fn(x) -> 2*x end,
-        bar: fn() -> :ok end] do
-      Dummy.foo 3
-      assert :meck.called Dummy, :foo, [3]
-      assert called Dummy.foo(3)
-      refute called Dummy.foo(2)
-      refute called Dummy.bar(3)
+    with_mock String,
+       [reverse: fn(x) -> 2*x end,
+        length: fn(x) -> :ok end] do
+      String.reverse 3
+      assert :meck.called String, :reverse, [3]
+      assert called String.reverse(3)
+      refute called String.reverse(2)
+      refute called String.length(3)
     end
   end
 
   test_with_mock "test_with_mock",
-    Dummy,
-    [get: fn(_x) -> :ok end] do
-    assert Dummy.get 3
-    assert called Dummy.get(3)
-    refute called Dummy.get(4)
+    String,
+    [reverse: fn(_x) -> :ok end] do
+    assert String.reverse 3
+    assert called String.reverse(3)
+    refute called String.reverse(4)
   end
 
   test_with_mock "passthrough", HashDict, [:passthrough],
     [] do
-    hd = HashDict.new([{:a, 1}])
+    hd = HashDict.put(HashDict.new(), :a, 1)
     assert HashDict.get(hd, :a) == 1
-    assert called HashDict.new([{:a, 1}])
+    assert called HashDict.new()
     assert called HashDict.get(hd, :a)
     refute called HashDict.get(hd, :b)
   end
