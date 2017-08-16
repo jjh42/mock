ddb03f825274481a8b8f6ecbbff521edc1b98df7
Daniel Olshansky <dolshansky@magicleap.com>
2017-08-16 16:48:29 -0400

    s/HashDict/Map in mock_setup_test.exs to remove warnings

--- test/mock_setup_test.exs
+++ test/mock_setup_test.exs
@@ -5,30 +5,30 @@ defmodule MockSetupTest do
   import Mock
 
   setup_with_mocks([
-    {HashDict, [], [get: fn(%{}, "http://example.com") -> "<html></html>" end]}
+    {Map, [:passthrough], [get: fn(%{}, "http://example.com") -> "<html></html>" end]}
   ]) do
     foo = "bar"
     {:ok, foo: foo}
   end
 
   test "setup_with_mocks" do
-    assert HashDict.get(%{}, "http://example.com") == "<html></html>"
+    assert Map.get(%{}, "http://example.com") == "<html></html>"
   end
 
-  test "setup_with_mocks with test context", %{foo: foo} do
-    assert HashDict.get(%{}, "http://example.com") == "<html></html>"
+  test "setup_with_mocks with test context", %{foo: "bar"} do
+    assert Map.get(%{}, "http://example.com") == "<html></html>"
   end
 
-  test_with_mock "setup_with_mocks respects test specific override", HashDict, [],
+  test_with_mock "setup_with_mocks respects test specific override", Map, [:passthrough],
     [get: fn(%{}, "http://example.com") -> "<html>override</html>" end] do
 
-    assert HashDict.get(%{}, "http://example.com") == "<html>override</html>"
+    assert Map.get(%{}, "http://example.com") == "<html>override</html>"
   end
 
-  test_with_mock "setup_with_mocks with test context respects test specific override", %{foo: foo},
-    HashDict, [], [get: fn(%{}, "http://example.com") -> "<html>override</html>" end] do
+  test_with_mock "setup_with_mocks with test context respects test specific override", %{foo: "bar"},
+    Map, [], [get: fn(%{}, "http://example.com") -> "<html>override</html>" end] do
 
-    assert HashDict.get(%{}, "http://example.com") == "<html>override</html>"
+    assert Map.get(%{}, "http://example.com") == "<html>override</html>"
   end
 
 end
@@ -38,17 +38,17 @@ defmodule MockSetupTestWithContext do
   import Mock
 
   setup_with_mocks([
-    {HashDict, [], [get: fn(%{}, "http://example.com") -> "<html></html>" end]}
+    {Map, [:passthrough], [get: fn(%{}, "http://example.com") -> "<html></html>" end]}
   ], context) do
     {:ok, test_string: Atom.to_string(context.test)}
   end
 
   test "setup_with_mocks with setup context" do
-    assert HashDict.get(%{}, "http://example.com") == "<html></html>"
+    assert Map.get(%{}, "http://example.com") == "<html></html>"
   end
 
   test "setup_with_mocks with setup context and test context", %{test_string: test_string} do
-    assert HashDict.get(%{}, "http://example.com") == "<html></html>"
+    assert Map.get(%{}, "http://example.com") == "<html></html>"
     assert test_string == "test setup_with_mocks with setup context and test context"
   end
 
