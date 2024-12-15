a3aecc4bf58363959611f75984d5423312ac4932
stjhimy <stjhimy@outlook.com>
2024-12-15 07:02:19 -0300

    Add .formatter.exs, run 'mix format'

--- test/mock_test.exs
+++ test/mock_test.exs
@@ -1,5 +1,4 @@
-Code.require_file "test_helper.exs", __DIR__
-
+Code.require_file("test_helper.exs", __DIR__)
 
 defmodule MockTest do
   use ExUnit.Case, async: false
@@ -12,18 +11,18 @@ defmodule MockTest do
 
   test "simple mock" do
     with_mock String,
-        [reverse: fn(x) -> 2*x end] do
+      reverse: fn x -> 2 * x end do
       assert String.reverse(3) == 6
     end
   end
 
   test "mock functions with multiple returns" do
-    with_mock(Map, [
+    with_mock(Map,
       get: fn
-        (%{}, "http://example.com") -> "<html>Hello from example.com</html>"
-        (%{}, "http://example.org") -> "<html>example.org says hi</html>"
+        %{}, "http://example.com" -> "<html>Hello from example.com</html>"
+        %{}, "http://example.org" -> "<html>example.org says hi</html>"
       end
-    ]) do
+    ) do
       assert Map.get(%{}, "http://example.com") == "<html>Hello from example.com</html>"
       assert Map.get(%{}, "http://example.org") == "<html>example.org says hi</html>"
     end
@@ -31,13 +30,8 @@ defmodule MockTest do
 
   test "multiple mocks" do
     with_mocks([
-      {Map,
-       [],
-       [get: fn(%{}, "http://example.com") -> "<html></html>" end]},
-      {String,
-       [],
-       [reverse: fn(x) -> 2*x end,
-        length: fn(_x) -> :ok end]}
+      {Map, [], [get: fn %{}, "http://example.com" -> "<html></html>" end]},
+      {String, [], [reverse: fn x -> 2 * x end, length: fn _x -> :ok end]}
     ]) do
       assert Map.get(%{}, "http://example.com") == "<html></html>"
       assert String.reverse(3) == 6
@@ -47,28 +41,28 @@ defmodule MockTest do
 
   test "mock functions with different arity" do
     with_mock String,
-      [slice: fn(string, _range)      -> string end,
-       slice: fn(string, _range, _len) -> string end]
-    do
+      slice: fn string, _range -> string end,
+      slice: fn string, _range, _len -> string end do
       assert String.slice("test", 1..3) == "test"
       assert String.slice("test", 1, 3) == "test"
     end
   end
 
   test "mock returns the result" do
-    result = with_mock String,
-    [reverse: fn(x) -> 2*x end] do
-      assert String.reverse(3) == 6
-      String.reverse(3)
-    end
+    result =
+      with_mock String,
+        reverse: fn x -> 2 * x end do
+        assert String.reverse(3) == 6
+        String.reverse(3)
+      end
+
     assert result == 6
   end
 
   test "mock returns multiples results in sequence" do
-    with_mock String, [
+    with_mock String,
       reverse: [in_series([:_], [:a, :b, :c])],
-      slice: [in_series([1, 2], [:x, :y, :z])]
-    ] do
+      slice: [in_series([1, 2], [:x, :y, :z])] do
       assert String.reverse(1) == :a
       assert String.reverse(1) == :b
       assert String.reverse(1) == :c
@@ -81,18 +75,18 @@ defmodule MockTest do
 
   test "called" do
     with_mock String,
-       [reverse: fn(x) -> 2*x end,
-        length: fn(_x) -> :ok end] do
-      String.reverse 3
-      assert :meck.called String, :reverse, [3]
-      assert called String.reverse(3)
-      refute called String.reverse(2)
-      refute called String.length(3)
+      reverse: fn x -> 2 * x end,
+      length: fn _x -> :ok end do
+      String.reverse(3)
+      assert :meck.called(String, :reverse, [3])
+      assert called(String.reverse(3))
+      refute called(String.reverse(2))
+      refute called(String.length(3))
     end
   end
 
   test "assert_called" do
-    with_mock String, [reverse: fn(x) -> 2*x end] do
+    with_mock String, reverse: fn x -> 2 * x end do
       String.reverse(3)
       assert_called(String.reverse(3))
 
@@ -110,7 +104,7 @@ defmodule MockTest do
   end
 
   test "assert_called_exactly" do
-    with_mock String, [reverse: fn(x) -> 2*x end] do
+    with_mock String, reverse: fn x -> 2 * x end do
       String.reverse(2)
       String.reverse(2)
       String.reverse(2)
@@ -128,7 +122,7 @@ defmodule MockTest do
   end
 
   test "assert_called_at_least" do
-    with_mock String, [reverse: fn(x) -> 2*x end] do
+    with_mock String, reverse: fn x -> 2 * x end do
       String.reverse(2)
       String.reverse(2)
       String.reverse(2)
@@ -148,7 +142,7 @@ defmodule MockTest do
 
   test "assert_not_called" do
     with_mock String,
-      [reverse: fn(x) -> 2*x end] do
+      reverse: fn x -> 2 * x end do
       String.reverse(3)
       String.reverse(3)
       assert_not_called(String.reverse(2))
@@ -165,48 +159,47 @@ defmodule MockTest do
   end
 
   test_with_mock "test_with_mock",
-    String,
-    [reverse: fn(_x) -> :ok end] do
-    assert String.reverse 3
-    assert called String.reverse(3)
-    refute called String.reverse(4)
+                 String,
+                 reverse: fn _x -> :ok end do
+    assert String.reverse(3)
+    assert called(String.reverse(3))
+    refute called(String.reverse(4))
   end
 
-  test_with_mock "test_with_mock with context", %{foo: foo}, String, [],
-    [reverse: fn(_x) -> :ok end] do
-    assert String.reverse 3
+  test_with_mock "test_with_mock with context", %{foo: foo}, String, [], reverse: fn _x -> :ok end do
+    assert String.reverse(3)
     assert foo == "bar"
-    assert called String.reverse(3)
-    refute called String.reverse(4)
+    assert called(String.reverse(3))
+    refute called(String.reverse(4))
   end
 
-  test_with_mock "passthrough option", Map, [:passthrough],
-    [] do
+  test_with_mock "passthrough option", Map, [:passthrough], [] do
     hd = Map.put(Map.new(), :a, 1)
     assert Map.get(hd, :a) == 1
-    assert called Map.new()
-    assert called Map.get(hd, :a)
-    refute called Map.get(hd, :b)
+    assert called(Map.new())
+    assert called(Map.get(hd, :a))
+    refute called(Map.get(hd, :b))
   end
 
   test_with_mock "passthrough in anon mock function", String, [:passthrough],
-    [reverse: fn x -> passthrough([x]) <> "!" end] do
+    reverse: fn x -> passthrough([x]) <> "!" end do
     assert String.reverse("xyz") == "zyx!"
-    assert called String.reverse("xyz")
+    assert called(String.reverse("xyz"))
   end
 
   test "restore after exception" do
     assert String.downcase("A") == "a"
+
     try do
       with_mock String,
-          [downcase: fn(x) -> x end] do
+        downcase: fn x -> x end do
         assert String.downcase("A") == "A"
         raise "some error"
       end
     rescue
       RuntimeError -> :ok
     end
+
     assert String.downcase("A") == "a"
   end
-
 end
